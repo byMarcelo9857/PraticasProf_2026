afc6894a4c0d09dc32a449c9e0071775a1aa1884
MARCELO RODRIGUES INOCENCIO <u26573@academico.cotuca.unicamp.br>
2026-08-11 22:07:26 -0300

    Adicionei linha 1 desenho)

--- aula.py
+++ aula.py
@@ -1,4 +1,9 @@
 nome = input("Digite seu nome ")
-idade = input("Digite sua idade ")
+idade = int(input("Digite sua idade "))
 print(f'Você digitou o nome {nome} e idade {idade}')
 
+print()
+print("Vamos desenhar")
+
+for x in range(idade):
+	print ("-", end="")
